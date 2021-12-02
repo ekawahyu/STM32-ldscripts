8a1927a665f4fec6784e78024dbbf93d948805ac
Ekawahyu Susilo <ekawahyu@icloud.com>
2021-12-02 00:20:24 -0800

    added initial linker file for STM32F407VG (for testing build with STM32F4Discovery)

--- convert.py
+++ convert.py
@@ -80,6 +80,54 @@ with open('STM32F3/STM32F3.csv', 'r') as csvfile:
                 csvfile.write(linker_templateCCRAM.safe_substitute(device=device, flash=flash, ram=ram,ccram=ccram))
 
 #=============================================================================#
+# STM32F4
+
+linker_template= Template('''/******************************************************************************
+	Memory Layout Linker File
+
+	Device : ${device}
+	- Flash       ${flash}kB
+	- RAM          ${ram}kB
+******************************************************************************/
+
+MEMORY{
+	RAM    (xrw)  : ORIGIN = 0x20000000, LENGTH = ${ram}K
+	FLASH  (rx)   : ORIGIN = 0x08000000, LENGTH = ${flash}K
+}''')
+
+linker_templateCCRAM= Template('''/******************************************************************************
+	Memory Layout Linker File
+
+	Device : ${device}
+	- Flash       ${flash}kB
+	- RAM          ${ram}kB
+	- CCRAM        ${ccram}kB
+******************************************************************************/
+
+MEMORY{
+	RAM    (xrw)  : ORIGIN = 0x20000000, LENGTH = ${ram}K
+	FLASH  (rx)   : ORIGIN = 0x08000000, LENGTH = ${flash}K
+	CCMRAM (xrw)  : ORIGIN = 0x10000000, LENGTH = ${ccram}K
+}''')
+
+
+with open('STM32F4/STM32F4.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    csvfile.readline() # dummy read, ignore header
+    for row in spamreader:
+        device = row[0]
+        flash  = row[1]
+        ram    = row[2]
+        ccram  = row[3] if( row[3] != '' ) else 0
+
+        if(ccram == 0):
+            with open('STM32F4/'+device + '.ld', 'w') as csvfile:
+                csvfile.write(linker_template.safe_substitute(device=device, flash=flash, ram=ram))
+        else:
+            with open('STM32F4/'+device + '.ld', 'w') as csvfile:
+                csvfile.write(linker_templateCCRAM.safe_substitute(device=device, flash=flash, ram=ram,ccram=ccram))
+
+#=============================================================================#
 # STM32F0
 
 linker_template= Template('''/******************************************************************************
